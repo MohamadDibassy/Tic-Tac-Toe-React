3779a396e1ece162d81f97da895be3dc0b1bddba
MohamadDibassy <mbdibass@buffalo.edu>
2021-08-19 20:02:24 -0400

    Implemented functionality

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import './App.css';
+import {useState, useEffect} from "react"; 
+import Square from "./Components/Square"
+import {Patterns} from "./Patterns"
+
+function App() {
+  const [board, setBoard] = useState(["","","","","","","","",""]);
+  const [player, setPlayer] = useState("X");
+  const [result, setResult] = useState({winner:"none", state: "none"});
+
+  useEffect(()=>{
+    checkwin();
+  },[board])
+
+  useEffect(()=>{
+    alert(`Game Finished! Winning Player:${result.winner}`);
+  },[result])
+
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((val,idx) =>{
+        if(idx == square && val == ""){
+          return player;
+        }
+        return val;
+      })
+    );
+    if(player == "X"){
+      setPlayer("O")
+    }else{
+      setPlayer("X");
+    }
+  };
+
+  const checkwin = () => {
+    Patterns.forEach((currPattern)=>{
+      const firstPlayer = board[currPattern[0]];
+      if(firstPlayer == "") return;
+      let foundWinningPattern = true;
+      currPattern.forEach((idx)=> {
+        if(board[idx] != firstPlayer) {
+          foundWinningPattern = false
+        }
+      })
+      if(foundWinningPattern){
+        setResult({winner: player, state: "Won"})
+      }
+    });
+  };
+
+
+
+
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="row">
+          <Square
+            val={board[0]}
+            chooseSquare={() => {
+              chooseSquare(0);
+            }}
+          />
+          alert(0); }} />
+          <Square
+            val={board[1]}
+            chooseSquare={() => {
+              chooseSquare(1);
+            }}
+          />
+          alert(1); }} />
+          <Square
+            val={board[2]}
+            chooseSquare={() => {
+              chooseSquare(2);
+            }}
+          />
+          alert(2); }} />
+        </div>
+        <div className="row">
+          <Square
+            val={board[3]}
+            chooseSquare={() => {
+              chooseSquare(3);
+            }}
+          />
+          alert(3); }} />
+          <Square
+            val={board[4]}
+            chooseSquare={() => {
+              chooseSquare(4);
+            }}
+          />
+          alert(4); }} />
+          <Square
+            val={board[5]}
+            chooseSquare={() => {
+              chooseSquare(5);
+            }}
+          />
+          alert(5); }} />
+        </div>
+        <div className="row">
+          <Square
+            val={board[6]}
+            chooseSquare={() => {
+              chooseSquare(6);
+            }}
+          />
+          alert(6);}} />
+          <Square
+            val={board[7]}
+            chooseSquare={() => {
+              chooseSquare(7);
+            }}
+          />
+          alert(7); }} />
+          <Square
+            val={board[8]}
+            chooseSquare={() => {
+              chooseSquare(8);
+            }}
+          />
+          alert(8);}} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
