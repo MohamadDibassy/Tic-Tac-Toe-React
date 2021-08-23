f80dd4c6950cc8ab0b3bf03f9eaab3873e30fe1d
MohamadDibassy <mbdibass@buffalo.edu>
2021-08-23 00:03:14 -0400

    Implemented square component

--- src/App.js
+++ src/App.js
@@ -1,75 +1,77 @@
-import './App.css';
-import {useState, useEffect} from "react"; 
-import Square from "./Components/Square"
-import {Patterns} from "./Patterns"
+import "./App.css";
+import { useState, useEffect } from "react";
+import Square from "./Components/Square";
+import { Patterns } from "./Patterns";
 
 function App() {
-  const [board, setBoard] = useState(["","","","","","","","",""]);
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState("O");
-  const [result, setResult] = useState({winner:"none", state: "none"});
+  const [result, setResult] = useState({ winner: "none", state: "none" });
 
-  useEffect(()=>{
-    checkwin();
+  useEffect(() => {
+    checkWin();
     checkIfTie();
 
-        if (player == "X") {
-          setPlayer("O");
-        } else {
-          setPlayer("X");
-        }
-  },[board])
-
-  useEffect(()=>{
-    if(result.state != "none"){
-    alert(`Game Finished! Winning Player:${result.winner}`);
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
     }
-  },[result])
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state != "none") {
+      alert(`Game Finished! Winning Player: ${result.winner}`);
+      restartGame();
+    }
+  }, [result]);
 
   const chooseSquare = (square) => {
     setBoard(
-      board.map((val,idx) =>{
-        if(idx == square && val == ""){
+      board.map((val, idx) => {
+        if (idx == square && val == "") {
           return player;
         }
+
         return val;
       })
     );
-
   };
 
-  const checkwin = () => {
-    Patterns.forEach((currPattern)=>{
+  const checkWin = () => {
+    Patterns.forEach((currPattern) => {
       const firstPlayer = board[currPattern[0]];
-      if(firstPlayer == "") return;
+      if (firstPlayer == "") return;
       let foundWinningPattern = true;
-      currPattern.forEach((idx)=> {
-        if(board[idx] != firstPlayer) {
-          foundWinningPattern = false
+      currPattern.forEach((idx) => {
+        if (board[idx] != firstPlayer) {
+          foundWinningPattern = false;
         }
-      })
-      if(foundWinningPattern){
-        setResult({winner: player, state: "Won"})
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "Won" });
       }
     });
   };
 
-const checkIfTie = ()=> {
-  let filled = true;
-  board.forEach((square) =>{
-    if(square == "") {
-      filled = false;
-    }
-  })
-    if(filled){
-      setResult({winner: "No One", state: "Tied"});
-      restartGame();
-    };
-};
+  const checkIfTie = () => {
+    let filled = true;
+    board.forEach((square) => {
+      if (square == "") {
+        filled = false;
+      }
+    });
 
-const restartGame = () =>{
-  setBoard(["", "", "", "", "", "", "", "", ""]);
-  setPlayer("X");
-};
+    if (filled) {
+      setResult({ winner: "No One", state: "Tie" });
+    }
+  };
+
+  const restartGame = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setPlayer("O");
+  };
 
   return (
     <div className="App">
@@ -81,21 +83,18 @@ const restartGame = () =>{
               chooseSquare(0);
             }}
           />
-          alert(0); }} />
           <Square
             val={board[1]}
             chooseSquare={() => {
               chooseSquare(1);
             }}
           />
-          alert(1); }} />
           <Square
             val={board[2]}
             chooseSquare={() => {
               chooseSquare(2);
             }}
           />
-          alert(2); }} />
         </div>
         <div className="row">
           <Square
@@ -104,21 +103,18 @@ const restartGame = () =>{
               chooseSquare(3);
             }}
           />
-          alert(3); }} />
           <Square
             val={board[4]}
             chooseSquare={() => {
               chooseSquare(4);
             }}
           />
-          alert(4); }} />
           <Square
             val={board[5]}
             chooseSquare={() => {
               chooseSquare(5);
             }}
           />
-          alert(5); }} />
         </div>
         <div className="row">
           <Square
@@ -127,21 +123,18 @@ const restartGame = () =>{
               chooseSquare(6);
             }}
           />
-          alert(6);}} />
           <Square
             val={board[7]}
             chooseSquare={() => {
               chooseSquare(7);
             }}
           />
-          alert(7); }} />
           <Square
             val={board[8]}
             chooseSquare={() => {
               chooseSquare(8);
             }}
           />
-          alert(8);}} />
         </div>
       </div>
     </div>
